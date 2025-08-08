63834ecba2362b1c712ecd0887f17d357a9cc008
Rodrigo Silva <r.fernando.08@outlook.com>
2025-08-08 17:35:09 -0300

    Versão 0.2

--- app.py
+++ app.py
@@ -6,4 +6,17 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def home():
     return render_template('index.html')
 
+@app.route('/comunidade')
+def comunidade():
+    return render_template('comunidade.html')
+
+@app.route('/navegacao')
+    return render_template('navegacao.html')
+
+@app.route('/quemsomos')
+    return render_template('quemsomos.html')
+
+if __name__ == '__main__':
+    app.run()
+
 # Adicione outras rotas se necessário
