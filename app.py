f4cbefaef8c1325fe53661559d9ff065ed84fec8
Rodrigo Silva <r.fernando.08@outlook.com>
2025-08-08 17:21:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask, render_template
 
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Adicione outras rotas se necessário
