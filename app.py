0bc131f3daf251f980cbfdd9cdc5483dbd40be5d
Rodrigo Silva <r.fernando.08@outlook.com>
2025-08-11 16:41:01 -0300

    V 0.3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, render_template_string
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -6,7 +6,19 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/usercreation')
+def user_creation():
+    return render_template_string('''
+        <form action="/create_user" method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Create User</button>
+        </form>
+    ''')
 
-# Adicione outras rotas se necessário
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return f"Submitted Username: {username}, Password: {password}"
+
