67e925bfc01135a452c2051a536d36fd4173b004
Rodrigo Silva <r.fernando.08@outlook.com>
2025-08-11 17:03:05 -0300

    v 4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('index.html')
 
